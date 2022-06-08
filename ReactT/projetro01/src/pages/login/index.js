4101ee560de600d36cfdeb7623d121a5d84be644
jonatas da silva prado <jonatasprado445@gmail.com>
2022-06-08 17:55:35 -0300

    tela de login quase okay falta a segurança

--- ReactT/projetro01/src/pages/login/index.js
+++ ReactT/projetro01/src/pages/login/index.js
@@ -1,16 +1,53 @@
 
-import './index.scss';
-
 import { Link } from 'react-router-dom';
-export default function Index(){
+import { login } from '../../api/usuarioAPI'
 
+import  LoadingBar  from 'react-top-loading-bar'
+import {useNavigate} from 'react-router-dom'
+import {useState, useRef} from 'react'
+
+import './index.scss'
+
+export default function Index() {
+
+    const [email, setEmail ] = useState('');
+    const [senha, setSenha] = useState('');
+    const [erro, setErro] = useState('');
+    const [carregando, setCarregando] = useState(false);
+
+    const navigate = useNavigate();
+    const ref = useRef();
+    
+     async function   entrarClick(){
+        ref.current.continuousStart();
+        setCarregando(true);
+
+         try{
+              const r = await login(email,senha);
+             
+
+            setTimeout(()=>{
+                navigate('/menu');
+            }, 3000);
+
+          
+
+        }catch (err) {
+            ref.current.complete();
+            setCarregando(false);
+            if(err.response.status===401){
+                setErro(err.response.data.erro);
+
+            }
+        }
+    }
 
 
     return(
     
        <main className='page-login' >
        
-                
+                <LoadingBar color='#f11946' ref={ref}/>
          
             <div className='body'>
          
@@ -25,14 +62,12 @@ export default function Index(){
 
                     <div className='div1'>
                         <h1>Logar</h1>
-                        <input type="text" placeholder="Nome"/> 
+                        <input type="text" placeholder="email" value={email} onChange={e => setEmail(e.target.value)} /> 
                     <br />   <br />
-                        <input type="password" placeholder="Senha"/>
+                        <input type="password" placeholder="Senha" value={senha} onChange={e => setSenha(e.target.value)}/>
                         <br />
                         <br />
-                        <button className='button'>
-                       <Link to="/menu"> Entrar</Link>
-                        </button>
+                        <button className='button' onClick={ entrarClick} disabled={carregando}>  Entrar </button>
                         
                     </div>
             
