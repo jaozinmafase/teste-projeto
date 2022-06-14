69bb439d2a53f15acfc1c6a8199dcc96a77c2d97
Matheusthebest <gamesmatheusplay9@gmail.com>
2022-06-14 11:55:52 -0300

    final 14/06

--- ReactT/projetro01/src/pages/login/index.js
+++ ReactT/projetro01/src/pages/login/index.js
@@ -30,6 +30,7 @@ export default function Index() {
 
          try{
               const r = await login(email,senha);
+              alert('foi')
               storage('asuario-logado', r);
               
 
